1f9d426bf6159e36414b292402f3c90785d78a81
EmmaWearing <emmak.wearing@gmail.com>
2017-11-13 17:28:33 +1100

    v1.0 First Commit

---
+++ Trailer - Backyard Snowdown_Unity/Assets/Scripts/DelayAnimation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayAnimation : MonoBehaviour {
+
+	public float timeToAnimation;
+
+	private bool hasPlayed=false;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timeToAnimation < Time.time && !hasPlayed) {
+			BeginAnimation ();
+			hasPlayed=true;
+		}
+	}
+
+	public void BeginAnimation(){
+		GetComponent<Animator> ().SetTrigger ("StartAnimation");
+	}
+}
